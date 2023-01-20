dbd3eefb9b4dd26d499f29da8eea15540d5e9d2d
ng <0xng@proton.me>
2023-01-20 03:12:56 +0200

    New analytics url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Script src="https://adata.createchecks.iamng.wtf/latest.js" />
+      <Script src="https://adata.createchecks.art/latest.js" />
       <noscript>
         {/* eslint-disable @next/next/no-img-element */}
         <img
-          src="https://adata.createchecks.iamng.wtf/noscript.gif"
+          src="https://adata.createchecks.art/noscript.gif"
           alt=""
           referrerPolicy="no-referrer-when-downgrade"
         />
