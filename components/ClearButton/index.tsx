1c2c3bfccbdebd6b195efec007bece1847585329
ng <0xng@proton.me>
2023-01-21 00:42:49 +0200

    Dark clear button

--- components/ClearButton/index.tsx
+++ components/ClearButton/index.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import { useBoardStore } from '../../store/board'
 
 const ClearButton = () => {
@@ -10,7 +11,11 @@ const ClearButton = () => {
   return (
     <button
       onClick={clearBoard}
-      className="rounded-full bg-white py-1 px-3 cursor-pointer text-sm text-grey w-max hover:bg-black hover:text-white transition-all duration-300"
+      className={clsx(
+        'rounded-full py-1 px-3 cursor-pointer text-sm transition-all duration-300',
+        'bg-white text-grey w-max hover:bg-black hover:text-white',
+        'dark:bg-black-canvas dark:text-grey dark:hover:bg-white dark:hover:text-black'
+      )}
     >
       Clear
     </button>
