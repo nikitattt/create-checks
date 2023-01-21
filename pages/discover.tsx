eb41265b445261115f3368a58837502921273add
ng <0xng@proton.me>
2023-01-21 05:50:14 +0200

    Add 3 derivs

--- pages/discover.tsx
+++ pages/discover.tsx
@@ -68,10 +68,59 @@ export async function getStaticProps() {
           author: {
             twitter: 'jalil_eth'
           }
+        },
+        {
+          name: 'Bricks',
+          type: 'NFT',
+          image:
+            'https://i.seadn.io/gcs/files/1775482a38a7a52063898bbda3c95331.jpg?auto=format&w=3840',
+          link: 'https://create.zora.co/collections/0x5b5b5470246992d519b02550213182cf0e0bf2c3',
+          author: {
+            twitter: 'aaraalto',
+            address: '',
+            ens: 'aaraalto.eth'
+          }
+        },
+        {
+          name: 'Noggled Checks - VV + Lil Nouns',
+          type: 'NFT',
+          image:
+            'https://i.seadn.io/gcs/files/cf4ee4de7790c745715d8f59970918dc.png?auto=format&w=1000',
+          link: 'https://create.zora.co/collections/0xa6d58aaaf07dcb19551654ff7617b234d75d53a5',
+          author: {
+            twitter: '',
+            address: '',
+            ens: 'lilsaturn.eth'
+          }
+        },
+        {
+          name: 'cc0hecks',
+          type: 'NFT',
+          image:
+            'https://i.seadn.io/gcs/files/500422d70efdbd3b91a27352819bb605.jpg?auto=format&w=1000',
+          link: 'https://create.zora.co/collections/0x8e359c7468ebf30512bcb5ca39c330d10a1505d8',
+          author: {
+            twitter: '',
+            address: '',
+            ens: 'ianbydesign.eth'
+          }
         }
       ]
     }
   }
 }
+
+// {
+//     name: '',
+//     type: 'NFT',
+//     image:
+//       '',
+//     link: '',
+//     author: {
+//       twitter: '',
+//       address: '',
+//       ens: ''
+//     }
+//   }
 
 export default Discover
