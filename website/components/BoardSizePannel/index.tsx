a8c5905c6806b07797023ba6805a3c9c50997179
ng <0xng@proton.me>
2023-01-30 18:51:09 +0200

    Cleanup and events record

--- website/components/BoardSizePannel/index.tsx
+++ website/components/BoardSizePannel/index.tsx
@@ -1,10 +1,6 @@
 import clsx from 'clsx'
-import { useState } from 'react'
 import { saEvent } from '../../scripts/events'
 import { useBoardStore } from '../../store/board'
-import { useMenuStore } from '../../store/menu'
-import { ColorMode } from '../../utils/colors'
-import { boardToImage } from '../../utils/image'
 
 const PredefinedSize = ({
   columns,
@@ -13,15 +9,13 @@ const PredefinedSize = ({
   columns: number
   rows: number
 }) => {
-  // const board = useBoardStore((state) => state.board)
   const setRows = useBoardStore((state) => state.setRows)
   const setColumns = useBoardStore((state) => state.setColumns)
-  // const darkMode = useMenuStore((state) => state.darkMode)
 
   const saveBoard = () => {
     setRows(rows)
     setColumns(columns)
-    // saEvent('save_image', { mode: darkMode ? 'dark' : 'light' })
+    saEvent('set_board_size', { size: `${rows}r x ${columns}c` })
   }
 
   return (
@@ -41,10 +35,6 @@ const PredefinedSize = ({
 const BoardSizePanel = () => {
   // const board = useBoardStore((state) => state.board)
   // const darkMode = useMenuStore((state) => state.darkMode)
-
-  const [boardXSize, setBoardXSize] = useState(8)
-  const [boardYSize, setBoardYSize] = useState(10)
-  const [expanded, setExpanded] = useState(false)
 
   const saveBoard = () => {
     // saEvent('save_image', { mode: darkMode ? 'dark' : 'light' })
@@ -73,17 +63,6 @@ const BoardSizePanel = () => {
         </div>
       </div>
     </div>
-    // <button
-    //   onClick={saveBoard}
-    //   className={clsx(
-    //     'rounded-full py-1 px-3 cursor-pointer text-sm transition-all duration-300',
-    //     'bg-white text-grey w-max hover:bg-black hover:text-white',
-    //     'dark:bg-black-canvas dark:text-grey dark:hover:bg-white dark:hover:text-black',
-    //     'shadow-lg sm:shadow-none'
-    //   )}
-    // >
-    //   Select Board Size
-    // </button>
   )
 }
 
